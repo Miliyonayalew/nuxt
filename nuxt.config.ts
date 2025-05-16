00e1502108c498857e3be52c76fedcfa56a705e3
Miliyonayalew <miliayalew@gmail.com>
2025-05-16 22:54:59 +0300

    Add ESLint configuration and update project dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  modules: ["@nuxt/eslint"],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  compatibilityDate: "2025-05-15",
+  css: ["@picocss/pico"],
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        quotes: "double",
+        commaDangle: "always-multiline",
+        indent: "tab",
+      },
+    },
+  },
+});
